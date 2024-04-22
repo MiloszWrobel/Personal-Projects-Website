bce014adc9076475a09e6a90652945bf15071721
MiloszWrobel <100210720+MiloszWrobel@users.noreply.github.com>
2024-04-22 12:40:46 +0100

    Created new components and updated existing components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
+import { useState } from "react";
 import NewProject from "./components/NewProject";
 import Sidebar from "./components/Sidebar";
+import NoProjectSelected from "./components/NoProjectSelected";
 
 function App() {
+  const [projectsState, SetProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  function handleStartAddProject() {
+    SetProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  }
+  let content;
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject />;
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
   return (
     <>
       <main className="h-screen my-8 flex gap-8">
-        <Sidebar />
-        <NewProject />
+        <Sidebar onStartAddProject={handleStartAddProject} />
+        {content}
       </main>
     </>
   );
